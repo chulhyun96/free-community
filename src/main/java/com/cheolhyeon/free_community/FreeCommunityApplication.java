2e84059505b3b112c451c7cfd543c9ec285227c2
chulhyun96 <park82602@gmail.com>
2025-02-25 22:52:10 +0900

    initail commit message

---
+++ src/main/java/com/cheolhyeon/free_community/FreeCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.cheolhyeon.free_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheolhyeon/free_community/FreeCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheolhyeon.free_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
