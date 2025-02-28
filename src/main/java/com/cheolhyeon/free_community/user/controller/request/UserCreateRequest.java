c57bc5c3abf72d4b98b5497f90dd7a864ce2225d
chulhyun96 <park82602@gmail.com>
2025-02-28 23:27:57 +0900

    style : NoArgs, AllArgs 생성자 애노테이션 추가

--- src/main/java/com/cheolhyeon/free_community/user/controller/request/UserCreateRequest.java
+++ src/main/java/com/cheolhyeon/free_community/user/controller/request/UserCreateRequest.java
@@ -1,8 +1,12 @@
 package com.cheolhyeon.free_community.user.controller.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
     private String username;
 }
