28e05e13319dc6ae5d118cad44f3ac6927148d84
chulhyun96 <park82602@gmail.com>
2025-02-28 22:16:57 +0900

    feat : UserCreate DTO 생성

--- src/main/java/com/cheolhyeon/free_community/user/controller/UserController.java
+++ src/main/java/com/cheolhyeon/free_community/user/controller/UserController.java
@@ -2,11 +2,19 @@ package com.cheolhyeon.free_community.user.controller;
 
 import com.cheolhyeon.free_community.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<?> create(@RequestBody UserCreateRequest request) {
+        return "";
+    }
 
 }
---
+++ src/main/java/com/cheolhyeon/free_community/user/controller/request/UserCreateRequest.java
@@ -0,0 +1,8 @@
+package com.cheolhyeon.free_community.user.controller.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserCreateRequest {
+    private String username;
+}
