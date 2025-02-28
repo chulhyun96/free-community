6bae6d26438a1edba0a170c10f131eb15e0e8e71
chulhyun96 <park82602@gmail.com>
2025-02-28 23:31:20 +0900

    feat : 새로운 User Create 기능 추가

--- src/main/java/com/cheolhyeon/free_community/user/controller/UserController.java
+++ src/main/java/com/cheolhyeon/free_community/user/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.cheolhyeon.free_community.user.controller;
 
+import com.cheolhyeon.free_community.user.controller.request.UserCreateRequest;
+import com.cheolhyeon.free_community.user.controller.response.UserCreateResponse;
+import com.cheolhyeon.free_community.user.domain.User;
 import com.cheolhyeon.free_community.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +16,8 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<?> create(@RequestBody UserCreateRequest request) {
-        return "";
+    public ResponseEntity<UserCreateResponse> create(@RequestBody UserCreateRequest request) {
+        User newUser = userService.create(request);
+        return ResponseEntity.ok(UserCreateResponse.from(newUser));
     }
-
 }
--- src/main/java/com/cheolhyeon/free_community/user/service/UserService.java
+++ src/main/java/com/cheolhyeon/free_community/user/service/UserService.java
@@ -1,13 +1,19 @@
 package com.cheolhyeon.free_community.user.service;
 
-import com.cheolhyeon.free_community.user.repository.UserRepository;
+import com.cheolhyeon.free_community.user.controller.request.UserCreateRequest;
+import com.cheolhyeon.free_community.user.domain.User;
+import com.cheolhyeon.free_community.user.service.port.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-
+    public User create(UserCreateRequest request) {
+        return userRepository.save(User.from(request));
+    }
 }
