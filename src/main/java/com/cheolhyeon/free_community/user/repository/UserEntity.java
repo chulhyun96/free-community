712f9990b1821ceecf649fcc1ebc349081e99df4
chulhyun96 <park82602@gmail.com>
2025-02-28 22:37:54 +0900

    feat : User의 활동 점수에 따라 고정 닉네임 신청이 가능한 프로젝트 정책으로 인한 필드 추가

--- src/main/java/com/cheolhyeon/free_community/user/repository/UserEntity.java
+++ src/main/java/com/cheolhyeon/free_community/user/repository/UserEntity.java
@@ -1,5 +1,6 @@
 package com.cheolhyeon.free_community.user.repository;
 
+import com.cheolhyeon.free_community.user.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,7 +17,26 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nickname;
+    private Long actionPoint;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    public static UserEntity from(User user) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.nickname = user.getNickname();
+        userEntity.actionPoint = user.getActionPoint();
+        userEntity.createdAt = user.getCreateAt();
+        userEntity.updatedAt = user.getUpdateAt();
+        return userEntity;
+    }
+
+    public User toModel() {
+        return User.builder()
+                .id(id)
+                .nickname(nickname)
+                .actionPoint(actionPoint)
+                .createAt(createdAt)
+                .updateAt(updatedAt)
+                .build();
+    }
 }
