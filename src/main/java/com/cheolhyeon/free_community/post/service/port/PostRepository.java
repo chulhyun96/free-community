d9384a23622aa3825b821aec3c30ac341923af92
chulhyun96 <park82602@gmail.com>
2025-02-28 23:01:15 +0900

    style : 패키지 구조 변경

--- src/main/java/com/cheolhyeon/free_community/post/service/port/PostRepository.java
+++ src/main/java/com/cheolhyeon/free_community/post/service/port/PostRepository.java
@@ -1,5 +1,10 @@
 package com.cheolhyeon.free_community.post.service.port;
 
+import com.cheolhyeon.free_community.post.domain.Post;
+import com.cheolhyeon.free_community.post.repository.entity.PostEntity;
+import com.cheolhyeon.free_community.user.repository.entity.UserEntity;
+
 public interface PostRepository {
 
+    Post save(Post newPost);
 }
