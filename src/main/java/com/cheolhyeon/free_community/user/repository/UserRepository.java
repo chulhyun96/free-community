82abc19ed6b3b74d8c8148eb245319166d4751ae
chulhyun96 <park82602@gmail.com>
2025-02-28 22:14:44 +0900

    feat : UserEntity 및 User 도메인 정의, 레이어드 아키텍처 생성

---
+++ src/main/java/com/cheolhyeon/free_community/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.cheolhyeon.free_community.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
