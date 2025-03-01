1ce29b0e36915fe2f653e82b37922e2d57cc069d
chulhyun96 <park82602@gmail.com>
2025-03-01 17:19:49 +0900

    feat : 유저 닉네임 에러상태 추가

--- src/main/java/com/cheolhyeon/free_community/user/exception/UserErrorStatus.java
+++ src/main/java/com/cheolhyeon/free_community/user/exception/UserErrorStatus.java
@@ -10,6 +10,10 @@ public enum UserErrorStatus implements ErrorStatus {
             HttpStatus.NOT_FOUND.value(),
             HttpStatus.NOT_FOUND.getReasonPhrase(),
             "유저 정보를 다시 확인해주세요."
+    ), INVALID_NICKNAME_LENGTH(
+            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.BAD_REQUEST.getReasonPhrase(),
+            "닉네임은 1~10자 사이여야 합니다."
     );
 
     private final int errorCode;
