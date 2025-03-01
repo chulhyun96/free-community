e46a28d4dff6283d82bcf3474181358b7d73dbac
chulhyun96 <park82602@gmail.com>
2025-03-01 17:20:12 +0900

    feat : update 메서드 추가

--- src/main/java/com/cheolhyeon/free_community/user/service/port/UserRepository.java
+++ src/main/java/com/cheolhyeon/free_community/user/service/port/UserRepository.java
@@ -2,7 +2,13 @@ package com.cheolhyeon.free_community.user.service.port;
 
 
 import com.cheolhyeon.free_community.user.domain.User;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface UserRepository  {
     User save(User from);
+
+    User findById(Long writerId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    User update(User user);
 }
