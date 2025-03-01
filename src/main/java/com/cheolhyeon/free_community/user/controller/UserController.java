1b456ff538a76cf5a052d2affb43021e7f86fe02
chulhyun96 <park82602@gmail.com>
2025-03-01 17:18:06 +0900

    feat : 조회, 업데이트 매핑 추가

--- src/main/java/com/cheolhyeon/free_community/user/controller/UserController.java
+++ src/main/java/com/cheolhyeon/free_community/user/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.cheolhyeon.free_community.user.controller;
 
 import com.cheolhyeon.free_community.user.controller.request.UserCreateRequest;
+import com.cheolhyeon.free_community.user.controller.request.UserUpdateRequest;
 import com.cheolhyeon.free_community.user.controller.response.UserCreateResponse;
+import com.cheolhyeon.free_community.user.controller.response.UserDetailResponse;
 import com.cheolhyeon.free_community.user.domain.User;
 import com.cheolhyeon.free_community.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +19,17 @@ public class UserController {
     public ResponseEntity<UserCreateResponse> create(@RequestBody UserCreateRequest request) {
         User newUser = userService.create(request);
         return ResponseEntity.ok(UserCreateResponse.from(newUser));
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserDetailResponse> readById(@PathVariable Long id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok(UserDetailResponse.from(user));
+    }
+
+    @PatchMapping("/users/{id}")
+    public ResponseEntity<?> updateById(@PathVariable Long id, @RequestBody UserUpdateRequest request) {
+        User update = userService.update(id, request);
+        return ResponseEntity.ok(UserDetailResponse.from(update));
     }
 }
