c82c28db46b74898187ce0a953dc678e3526bc3f
chulhyun96 <park82602@gmail.com>
2025-03-01 20:36:13 +0900

    test : 도메인 로직 테스트 코드

--- src/main/java/com/cheolhyeon/free_community/user/controller/request/UserUpdateRequest.java
+++ src/main/java/com/cheolhyeon/free_community/user/controller/request/UserUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.cheolhyeon.free_community.user.controller.request;
 
-import com.cheolhyeon.free_community.user.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,10 +9,4 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserUpdateRequest {
     private String nickname;
-
-    public static UserUpdateRequest from(User user) {
-        UserUpdateRequest request = new UserUpdateRequest();
-        request.nickname = user.getNickname();
-        return request;
-    }
 }
---
+++ src/test/java/com/cheolhyeon/free_community/user/domain/UserTest.java
@@ -0,0 +1,29 @@
+package com.cheolhyeon.free_community.user.domain;
+
+import com.cheolhyeon.free_community.user.controller.request.UserCreateRequest;
+import com.cheolhyeon.free_community.user.controller.request.UserUpdateRequest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @Test
+    void from() {
+        UserCreateRequest test = new UserCreateRequest("test");
+        User from = User.from(test);
+
+        assertThat(from.getNickname()).isEqualTo(test.getUsername());
+        assertThat(from.getActionPoint()).isEqualTo(0L);
+    }
+
+    @Test
+    void updateNickname() {
+        UserUpdateRequest updateTest = new UserUpdateRequest("update_test");
+
+        User user = new User();
+        user.updateNickname(updateTest);
+
+        assertThat(user.getNickname()).isEqualTo(updateTest.getNickname());
+    }
+}
