37f83d097fcebaea3db51e1180e8370942baf264
chulhyun96 <park82602@gmail.com>
2025-03-06 20:23:58 +0900

    feat : findByIdAndUserId를 통해 특정 User가 작성한 Post 불러오기

--- src/main/java/com/cheolhyeon/free_commnunity_1/post/repository/PostRepository.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/post/repository/PostRepository.java
@@ -1,7 +1,14 @@
 package com.cheolhyeon.free_commnunity_1.post.repository;
 
 import com.cheolhyeon.free_commnunity_1.post.repository.entity.PostEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    @Query("select p from PostEntity p where p.id = :postId and p.userId = :userId")
+    Optional<PostEntity> findByIdAndUserId(@Param("postId") Long postId ,@Param("userId") Long userId);
 }
---
+++ src/test/java/com/cheolhyeon/free_commnunity_1/post/repository/PostRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.cheolhyeon.free_commnunity_1.post.repository;
+
+import com.cheolhyeon.free_commnunity_1.post.repository.entity.PostEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@Slf4j
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PostRepositoryTest {
+   @Autowired
+   private PostRepository postRepository;
+
+   @Test
+   @DisplayName("특정 User 작성한 Post 찾아오기")
+   void findByIdAndUserId() {
+       PostEntity postEntity = postRepository.findByIdAndUserId(1L, 1L).get();
+       log.info("PostEntity: {}", postEntity);
+   }
+}
