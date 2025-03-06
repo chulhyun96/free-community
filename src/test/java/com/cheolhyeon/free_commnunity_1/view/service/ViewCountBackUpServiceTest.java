1df8710f45ea12ccfafc73ab75703d7f3cf89155
chulhyun96 <park82602@gmail.com>
2025-03-06 19:42:50 +0900

    style : import문 제거

--- src/test/java/com/cheolhyeon/free_commnunity_1/view/service/ViewCountBackUpServiceTest.java
+++ src/test/java/com/cheolhyeon/free_commnunity_1/view/service/ViewCountBackUpServiceTest.java
@@ -8,11 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
