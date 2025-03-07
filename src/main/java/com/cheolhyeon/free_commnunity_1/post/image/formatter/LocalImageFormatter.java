5dad9b91c2853a81eab7fd35408901cffc7f34e1
chulhyun96 <park82602@gmail.com>
2025-03-07 22:19:51 +0900

    refactor : JsonProcessingException은 Spring에서 처리가 가능한 에러여서 근야 던지는 방향으로 변경

--- src/main/java/com/cheolhyeon/free_commnunity_1/post/image/formatter/LocalImageFormatter.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/post/image/formatter/LocalImageFormatter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,34 +15,37 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class LocalImageFormatter implements ImageStrategy {
-    private final static String BASE_PATH = "/Users/cheolhyeon/desktop"; // S3 이용시 S3 엔드포인트로 변경
+
+    @Value("${image.base-path}")
+    private String basePath;
     private final ObjectMapper mapper;
 
     @Override
-    public String formatToSave(List<MultipartFile> images) {
+    public String formatToSave(List<MultipartFile> images) throws JsonProcessingException {
         if (images == null || images.isEmpty()) {
             return "[]";
         }
-        return getJsonPathsList(images).toString();
+        return toJson(getJsonPathsList(images));
     }
 
+
     @Override
-    public String formatToSave(List<MultipartFile> newImages, List<String> deletedImages, String existingImages) {
-        // 기존 이미지 JSON을 List<String>으로 변환
+    public String formatToSave(List<MultipartFile> newImages, List<String> deletedImages, String existingImages) throws JsonProcessingException {
+        // 1.기존 이미지 String 타입의 JSON 포맷을 List<String>으로 변환
         List<String> existingImagePaths = parseJsonToList(existingImages);
 
-        // 1. 기존 이미지에서 삭제할 이미지 제거, 삭제할 이미지가 없을 경우 리스트로 변환 후 제거
+        // 2. 기존 이미지에서 삭제할 이미지 제거, 삭제할 이미지가 없을 경우 String 리스트로 변환 후 제거
         List<String> safeDeletedImages = Optional.ofNullable(deletedImages).orElseGet(List::of);
         existingImagePaths.removeAll(safeDeletedImages);
 
-        // 2. 새로운 이미지 추가, newImages가 null일 경우 빈 리스트로 변환 후 새로운 이미지 추가
+        // 3. 새로운 이미지 추가, newImages null 일 경우 빈 리스트로 변환 후 새로운 이미지 추가
         List<MultipartFile> safeNewImages = Optional.ofNullable(newImages).orElseGet(List::of);
         if (notEmpty(safeNewImages)) {
             List<String> newImagePaths = getJsonPathsList(safeNewImages);
             existingImagePaths.addAll(newImagePaths);
         }
 
-        // 3. 최종 리스트 JSON으로 변환하여 반환
+        // 3. 최종 String 리스트를 JSON 포맷으로 변환하여 반환
         return toJson(existingImagePaths);
     }
 
@@ -51,24 +55,15 @@ public class LocalImageFormatter implements ImageStrategy {
 
     private List<String> getJsonPathsList(List<MultipartFile> images) {
         return images.stream()
-                .map(image -> BASE_PATH + File.separator + image.getOriginalFilename())
+                .map(image -> basePath + File.separator + image.getOriginalFilename())
                 .toList();
     }
 
-    private List<String> parseJsonToList(String json) {
-        try {
-            return mapper.readValue(json, new TypeReference<>() {
-            });
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to parse JSON", e);
-        }
+    private List<String> parseJsonToList(String json) throws JsonProcessingException {
+        return mapper.readValue(json, new TypeReference<>() {});
     }
 
-    private String toJson(List<String> paths) {
-        try {
-            return mapper.writeValueAsString(paths);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to convert to JSON", e);
-        }
+    private String toJson(List<String> paths) throws JsonProcessingException {
+        return mapper.writeValueAsString(paths);
     }
 }
