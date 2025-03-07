34caadd3219cffd3137c2fbf59429133248521ad
chulhyun96 <park82602@gmail.com>
2025-03-07 22:19:59 +0900

    refactor : JsonProcessingException은 Spring에서 처리가 가능한 에러여서 근야 던지는 방향으로 변경

--- src/main/java/com/cheolhyeon/free_commnunity_1/post/image/formatter/ImageStrategy.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/post/image/formatter/ImageStrategy.java
@@ -1,12 +1,13 @@
 package com.cheolhyeon.free_commnunity_1.post.image.formatter;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ImageStrategy {
-    String formatToSave(List<MultipartFile> images);
+    String formatToSave(List<MultipartFile> images) throws JsonProcessingException;
 
-    String formatToSave(List<MultipartFile> images, List<String> deletedImages, String existingImages);
+    String formatToSave(List<MultipartFile> images, List<String> deletedImages, String existingImages) throws JsonProcessingException;
 }
