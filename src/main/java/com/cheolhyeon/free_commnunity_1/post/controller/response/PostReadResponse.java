ae10ee1d7d6fff2d0fad18f247700f7edb4550b0
chulhyun96 <park82602@gmail.com>
2025-03-09 00:39:20 +0900

    feat : 현재 게시글의 댓글, 댓글 갯수 포함 추가

--- src/main/java/com/cheolhyeon/free_commnunity_1/post/controller/response/PostReadResponse.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/post/controller/response/PostReadResponse.java
@@ -1,10 +1,12 @@
 package com.cheolhyeon.free_commnunity_1.post.controller.response;
 
 
+import com.cheolhyeon.free_commnunity_1.comment.controller.reponse.CommentReadResponse;
 import com.cheolhyeon.free_commnunity_1.post.domain.Post;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -18,10 +20,11 @@ public class PostReadResponse {
     private String imageUrl;
     private String categoryName;
     private Long viewCount;
-    private String comment;
+    private List<CommentReadResponse> comments;
+    private int commentCount;
     private LocalDateTime updatedAt;
 
-    public static PostReadResponse from(Post post, Long currentViewCount, String writer, String categoryName) {
+    public static PostReadResponse from(Post post, Long currentViewCount, String writer, String categoryName, List<CommentReadResponse> comments, int commentCount) {
         PostReadResponse response = new PostReadResponse();
         response.title = post.getTitle();
         response.content = post.getContent();
@@ -30,6 +33,8 @@ public class PostReadResponse {
         response.categoryName = categoryName;
         response.viewCount = currentViewCount;
         response.updatedAt = post.getUpdatedAt();
+        response.commentCount = commentCount;
+        response.comments = comments;
         return response;
     }
 }
