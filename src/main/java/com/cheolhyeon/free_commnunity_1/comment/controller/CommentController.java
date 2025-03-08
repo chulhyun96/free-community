2dc7034648d9e41c3bd15a84ea13ede3d27e8fc2
chulhyun96 <park82602@gmail.com>
2025-03-09 02:45:13 +0900

    feat : Comment Update

--- src/main/java/com/cheolhyeon/free_commnunity_1/comment/controller/CommentController.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/comment/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.cheolhyeon.free_commnunity_1.comment.controller;
 
 import com.cheolhyeon.free_commnunity_1.comment.controller.request.CommentCreateRequest;
+import com.cheolhyeon.free_commnunity_1.comment.controller.request.CommentUpdateRequest;
 import com.cheolhyeon.free_commnunity_1.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,10 +14,20 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<HttpStatus> createComment(
+    public ResponseEntity<HttpStatus> create(
             @PathVariable Long postId,
             @RequestBody CommentCreateRequest request) {
         commentService.create(postId, request);
         return ResponseEntity.ok(HttpStatus.OK);
     }
+
+    @PatchMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<HttpStatus> update(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            @RequestBody CommentUpdateRequest request) {
+        commentService.update(postId, commentId, request);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
 }
