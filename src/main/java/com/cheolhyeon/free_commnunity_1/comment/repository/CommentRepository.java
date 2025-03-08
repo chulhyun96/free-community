dc01257d40e36e608d4ab8f438481dc20639ea5c
chulhyun96 <park82602@gmail.com>
2025-03-08 14:55:15 +0900

    feat : PostId로 CommentEntity 조회 쿼리 메서드 작성

--- src/main/java/com/cheolhyeon/free_commnunity_1/comment/repository/CommentRepository.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/comment/repository/CommentRepository.java
@@ -3,6 +3,10 @@ package com.cheolhyeon.free_commnunity_1.comment.repository;
 import com.cheolhyeon.free_commnunity_1.comment.repository.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findByPostIdOrderByCreatedAtAsc(Long postId);
+
 }
 
