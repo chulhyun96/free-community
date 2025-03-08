2d9d9898e1694a7c66288a8c683c61037b288ab7
chulhyun96 <park82602@gmail.com>
2025-03-09 02:33:20 +0900

    refactor : API 수정

--- src/main/java/com/cheolhyeon/free_commnunity_1/comment/controller/CommentController.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/comment/controller/CommentController.java
@@ -5,19 +5,18 @@ import com.cheolhyeon.free_commnunity_1.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping("/comments")
-    public ResponseEntity<?> createComment(
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<HttpStatus> createComment(
+            @PathVariable Long postId,
             @RequestBody CommentCreateRequest request) {
-        commentService.create(request);
-        return ResponseEntity.ok(HttpStatus.OK.value());
+        commentService.create(postId, request);
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 }
