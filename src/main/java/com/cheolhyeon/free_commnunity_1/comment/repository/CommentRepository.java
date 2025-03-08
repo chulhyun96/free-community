adf821eb557833ba7c1734659b8a1e0e0d691223
chulhyun96 <park82602@gmail.com>
2025-03-09 02:46:05 +0900

    feat : findByPostIdAndCommentId 쿼리 메서드 작성

--- src/main/java/com/cheolhyeon/free_commnunity_1/comment/repository/CommentRepository.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/comment/repository/CommentRepository.java
@@ -4,9 +4,11 @@ import com.cheolhyeon.free_commnunity_1.comment.repository.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     List<CommentEntity> findByPostIdOrderByCreatedAtAsc(Long postId);
 
+    Optional<CommentEntity> findByPostIdAndCommentId(Long postId, Long commentId);
 }
 
