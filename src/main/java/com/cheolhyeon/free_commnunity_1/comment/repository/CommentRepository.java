e6e629600ff024468a66f5dcaddb74bff547c14b
chulhyun96 <park82602@gmail.com>
2025-03-11 22:51:40 +0900

    feat : 해당 Comment가 루트인지 아닌지 판별하기 위한 count 쿼리

--- src/main/java/com/cheolhyeon/free_commnunity_1/comment/repository/CommentRepository.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/comment/repository/CommentRepository.java
@@ -1,7 +1,9 @@
 package com.cheolhyeon.free_commnunity_1.comment.repository;
 
 import com.cheolhyeon.free_commnunity_1.comment.repository.entity.CommentEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +12,15 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     List<CommentEntity> findByPostIdOrderByCreatedAtAsc(Long postId);
 
     Optional<CommentEntity> findByPostIdAndCommentId(Long postId, Long commentId);
+
+    @Query(
+            value = "select count(*) from (" +
+                    "select comment_id from comment " +
+                    "where post_id = :postId and parent_comment_id = :parentCommentId " +
+                    "limit :limit" +
+                    ") as cci",
+            nativeQuery = true
+    )
+    int countBy(@Param("postId") Long postId, @Param("parentCommentId") Long parentCommentId, @Param("limit") long limit);
 }
 
