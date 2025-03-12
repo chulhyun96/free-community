697da180bc05fa38936f8115aba4cf690c80c6b9
chulhyun96 <park82602@gmail.com>
2025-03-12 20:20:04 +0900

    feat : 댓글의 좋아요 증가 및 감소 전에 Redis에 특정 User가 특정 Comment에 좋아요 관련 작업을 한적이 있는지 검사

---
+++ src/main/java/com/cheolhyeon/free_commnunity_1/commentlike/repository/CommentLikeQueryRedisRepository.java
@@ -0,0 +1,34 @@
+package com.cheolhyeon.free_commnunity_1.commentlike.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentLikeQueryRedisRepository {
+    private static final String KEY = "comment:user:%s:comment:%s:like";
+    private final StringRedisTemplate commentLikeRedisTemplate;
+
+    public boolean isLikedByUserIdAndCommentId(Long userId, Long commentId) {
+        String key = generateKey(userId, commentId);
+        return contains(key);
+    }
+    public void insert(Long userId, Long commentId) {
+        String key = generateKey(userId, commentId);
+        commentLikeRedisTemplate.opsForValue().set(key, "");
+    }
+
+    public void delete(Long userId, Long commentId) {
+        String key = generateKey(userId, commentId);
+        commentLikeRedisTemplate.delete(key);
+    }
+
+    private boolean contains(String key) {
+        return Boolean.TRUE.equals(commentLikeRedisTemplate.hasKey(key));
+    }
+
+    private String generateKey(Long userId, Long commentId) {
+        return KEY.formatted(userId, commentId);
+    }
+}
