6bd20fc8f8c42b50c6d69a906f8f52647ce34ad2
chulhyun96 <park82602@gmail.com>
2025-03-12 20:20:16 +0900

    feat : 댓글의 좋아요 증가 및 감소

---
+++ src/main/java/com/cheolhyeon/free_commnunity_1/commentlike/repository/CommentLikeRedisRepository.java
@@ -0,0 +1,32 @@
+package com.cheolhyeon.free_commnunity_1.commentlike.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentLikeRedisRepository {
+    private static final String KEY = "comment_like_count:%s";
+    private final StringRedisTemplate commentLikeRedisTemplate;
+
+    public void increment(Long commentId) {
+        String key = generateKey(commentId);
+        commentLikeRedisTemplate.opsForValue().increment(key);
+    }
+
+    public void decrement(Long commentId) {
+        String key = generateKey(commentId);
+        commentLikeRedisTemplate.opsForValue().decrement(key);
+    }
+
+    public Long getCurrentCommentLikeCount(Long commentId) {
+        String key = generateKey(commentId);
+        String result = commentLikeRedisTemplate.opsForValue().get(key);
+        return result == null ? 0L : Long.parseLong(result);
+    }
+
+    private String generateKey(Long commentId) {
+        return KEY.formatted(commentId);
+    }
+}
