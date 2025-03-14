05322a44dfbc8c602bbd2e42bf6e98bdad0a945a
chulhyun96 <park82602@gmail.com>
2025-03-14 18:39:56 +0900

    refactor : LocalDateTime을 외부에서 받을 수 있도록 변경

--- src/main/java/com/cheolhyeon/free_commnunity_1/user/controller/UserController.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/user/controller/UserController.java
@@ -10,26 +10,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public ResponseEntity<?> create(@RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserCreateResponse> create(@RequestBody UserCreateRequest request) {
         User user = userService.create(request);
         return ResponseEntity.ok(UserCreateResponse.from(user));
     }
 
     @GetMapping("/users/{userId}")
-    public ResponseEntity<?> readById(@PathVariable Long userId) {
+    public ResponseEntity<UserReadResponse> readById(@PathVariable Long userId) {
         User user = userService.readById(userId);
         return ResponseEntity.ok(UserReadResponse.from(user));
     }
 
     @PatchMapping("/users/{userId}")
-    public ResponseEntity<?> updateById(@PathVariable Long userId, @RequestBody UserUpdateRequest request) {
-        User user = userService.updateById(userId, request);
+    public ResponseEntity<UserReadResponse> updateById(@PathVariable Long userId, @RequestBody UserUpdateRequest request) {
+        LocalDateTime updatedAt = LocalDateTime.now();
+        User user = userService.updateById(userId, request, updatedAt);
         return ResponseEntity.ok(UserReadResponse.from(user));
     }
 }
