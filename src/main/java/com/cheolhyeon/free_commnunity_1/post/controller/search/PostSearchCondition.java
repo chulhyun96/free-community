433e568eddf7140d8a8253bd126b9ee3da211f5a
chulhyun96 <park82602@gmail.com>
2025-03-15 21:55:29 +0900

    feat : 게시글 검색 조건 DTO 클래스 생성

---
+++ src/main/java/com/cheolhyeon/free_commnunity_1/post/controller/search/PostSearchCondition.java
@@ -0,0 +1,13 @@
+package com.cheolhyeon.free_commnunity_1.post.controller.search;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class PostSearchCondition {
+    private String title;
+    private String categoryName;
+}
