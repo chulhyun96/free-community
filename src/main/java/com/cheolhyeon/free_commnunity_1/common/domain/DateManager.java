86ed0f0bc6fcf27a57d8050b7827c6d5e2a8a06c
chulhyun96 <park82602@gmail.com>
2025-03-20 15:33:36 +0900

    feat :  Date를 관리할 객체

---
+++ src/main/java/com/cheolhyeon/free_commnunity_1/common/domain/DateManager.java
@@ -0,0 +1,19 @@
+package com.cheolhyeon.free_commnunity_1.common.domain;
+
+import java.time.LocalDateTime;
+
+public class DateManager {
+    private final LocalDateTime localDateTime;
+
+    public DateManager(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public LocalDateTime getMinusMonthsAsLocalDate(long months) {
+        return localDateTime.toLocalDate().minusMonths(months).atStartOfDay();
+    }
+
+    public LocalDateTime getLocalDateTimeNow() {
+        return localDateTime.toLocalDate().atStartOfDay();
+    }
+}
