836e51b3b8702f82d8895d22efb8d3a8c25cbe0b
chulhyun96 <park82602@gmail.com>
2025-03-20 17:27:30 +0900

    feat :  사용자의 게시글 정보와 댓글 정보를 불러오는 기능

--- src/main/java/com/cheolhyeon/free_commnunity_1/user/service/UserService.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/user/service/UserService.java
@@ -1,21 +1,34 @@
 package com.cheolhyeon.free_commnunity_1.user.service;
 
+import com.cheolhyeon.free_commnunity_1.comment.repository.CommentRepository;
+import com.cheolhyeon.free_commnunity_1.comment.repository.entity.CommentEntity;
+import com.cheolhyeon.free_commnunity_1.common.domain.DateManager;
+import com.cheolhyeon.free_commnunity_1.post.repository.PostRepository;
+import com.cheolhyeon.free_commnunity_1.post.repository.entity.PostEntity;
 import com.cheolhyeon.free_commnunity_1.user.controller.request.UserCreateRequest;
 import com.cheolhyeon.free_commnunity_1.user.controller.request.UserUpdateRequest;
+import com.cheolhyeon.free_commnunity_1.user.controller.response.CommentHistory;
+import com.cheolhyeon.free_commnunity_1.user.controller.response.PostHistory;
+import com.cheolhyeon.free_commnunity_1.user.controller.response.UserHistoryResponse;
 import com.cheolhyeon.free_commnunity_1.user.domain.User;
 import com.cheolhyeon.free_commnunity_1.user.repository.UserRepository;
 import com.cheolhyeon.free_commnunity_1.user.repository.entity.UserEntity;
+import com.cheolhyeon.free_commnunity_1.view.service.ViewCountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final ViewCountService viewCountService;
 
     public User create(UserCreateRequest request) {
         User user = User.from(request);
@@ -41,4 +54,45 @@ public class UserService {
         return model;
     }
 
+    @Transactional(readOnly = true)
+    public UserHistoryResponse getHistory(Long userId) {
+        DateManager dateManager = new DateManager(LocalDateTime.now());
+        LocalDateTime startDate = dateManager.getMinusMonthsAsLocalDate(1);
+        LocalDateTime endDate = dateManager.getLocalDateNow();
+
+        UserHistoryResponse userHistory = new UserHistoryResponse();
+        addPostHistory(userId, startDate, endDate, userHistory);
+        addCommentHistory(userId, startDate, endDate, userHistory);
+        return userHistory;
+    }
+
+    private void addPostHistory(Long userId, LocalDateTime startDate, LocalDateTime endDate, UserHistoryResponse userHistory) {
+        List<PostEntity> postsHistory = postRepository.findByUserIdAndDate(userId, startDate, endDate);
+        if (postsHistory.isEmpty()) {
+            return;
+        }
+        postsHistory.forEach(postEntity -> {
+            Long currentViewCount = viewCountService.getCurrentViewCount(postEntity.getId());
+            userHistory.addPostHistory(
+                    PostHistory.of(
+                            postEntity.getId(),
+                            postEntity.getTitle(),
+                            currentViewCount));
+        });
+    }
+
+    private void addCommentHistory(Long userId, LocalDateTime startDate, LocalDateTime endDate, UserHistoryResponse userHistory) {
+        List<CommentEntity> commentsHistory = commentRepository.findByUserIdAndDate(userId, startDate, endDate);
+        if (commentsHistory.isEmpty()) {
+            return;
+        }
+        commentsHistory.forEach(commentEntity -> {
+            String postTitle = userHistory.getPostTitle(commentEntity.getPostId());
+            userHistory.addCommentHistory(
+                    CommentHistory.of(
+                            commentEntity.getCommentId(),
+                            postTitle,
+                            commentEntity.getContent()));
+        });
+    }
 }
