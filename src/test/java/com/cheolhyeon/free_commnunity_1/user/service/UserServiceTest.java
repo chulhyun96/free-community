102a263db78cb6aa00e46e86bc3199b65bdf19c1
chulhyun96 <park82602@gmail.com>
2025-03-21 17:16:04 +0900

    test :  유저 게시글 및 댓글 이력과 관련된 테스트 코드 작성

--- src/test/java/com/cheolhyeon/free_commnunity_1/user/service/UserServiceTest.java
+++ src/test/java/com/cheolhyeon/free_commnunity_1/user/service/UserServiceTest.java
@@ -1,28 +1,46 @@
 package com.cheolhyeon.free_commnunity_1.user.service;
 
+import com.cheolhyeon.free_commnunity_1.comment.repository.CommentRepository;
+import com.cheolhyeon.free_commnunity_1.comment.repository.entity.CommentEntity;
+import com.cheolhyeon.free_commnunity_1.post.repository.PostRepository;
+import com.cheolhyeon.free_commnunity_1.post.repository.entity.PostEntity;
 import com.cheolhyeon.free_commnunity_1.user.controller.request.UserCreateRequest;
 import com.cheolhyeon.free_commnunity_1.user.controller.request.UserUpdateRequest;
+import com.cheolhyeon.free_commnunity_1.user.controller.response.UserHistoryResponse;
+import com.cheolhyeon.free_commnunity_1.user.domain.CommentHistory;
+import com.cheolhyeon.free_commnunity_1.user.domain.PostHistory;
 import com.cheolhyeon.free_commnunity_1.user.domain.User;
 import com.cheolhyeon.free_commnunity_1.user.repository.UserRepository;
 import com.cheolhyeon.free_commnunity_1.user.repository.entity.UserEntity;
+import com.cheolhyeon.free_commnunity_1.view.service.ViewCountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+    @Mock
+    PostRepository postRepository;
+
+    @Mock
+    CommentRepository commentRepository;
+
+    @Mock
+    ViewCountService viewCountService;
 
     @Mock
     UserRepository userRepository;
@@ -93,5 +111,171 @@ class UserServiceTest {
         verify(userRepository, times(1)).findById(userId);
         verify(entity, times(1)).toModel();
         verify(entity, times(1)).update(any(User.class));
+    }
+
+    @Test
+    @DisplayName("getHistory 호출 시 각각의 Entity의 Repository에서 쿼리가 발생한다")
+    void getHistoryOccurQuery() {
+        // given
+        Long userId = 1L;
+        given(postRepository.findByUserIdAndDate(eq(userId), any(LocalDateTime.class), any(LocalDateTime.class)))
+                .willReturn(Collections.emptyList());
+        given(commentRepository.findByUserIdAndDate(eq(userId), any(LocalDateTime.class), any(LocalDateTime.class)))
+                .willReturn(Collections.emptyList());
+
+        // when
+        userService.getHistory(userId);
+
+        // then:
+        then(postRepository).should().findByUserIdAndDate(eq(userId), any(LocalDateTime.class), any(LocalDateTime.class));
+        then(commentRepository).should().findByUserIdAndDate(eq(userId), any(LocalDateTime.class), any(LocalDateTime.class));
+    }
+
+    @Test
+    @DisplayName("게시글과 댓글 이력이 모두 없는 경우 아무 작업도 수행하지 않는다.")
+    void getHistoryNoReturn() {
+        //given
+
+        given(postRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(Collections.emptyList());
+
+        given(commentRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(Collections.emptyList());
+
+        try (MockedStatic<PostHistory> postHistoryStatic = mockStatic(PostHistory.class);
+             MockedStatic<CommentHistory> commentHistoryStatic = mockStatic(CommentHistory.class)) {
+            //when
+            userService.getHistory(1L);
+
+            //then
+            postHistoryStatic.verifyNoInteractions();
+            commentHistoryStatic.verifyNoInteractions();
+            then(postRepository).should(times(1))
+                    .findByUserIdAndDate(anyLong(), any(LocalDateTime.class), any(LocalDateTime.class));
+            then(commentRepository).should(times(1))
+                    .findByUserIdAndDate(anyLong(), any(LocalDateTime.class), any(LocalDateTime.class));
+        }
+    }
+
+    @Test
+    @DisplayName("게시글 이력만 존재하는 경우 PostHistory.from과 addHistory가 호출된다.")
+    void getHistoryOnlyPost() {
+        //given
+        PostEntity dummyPost = PostEntity.builder().build();
+        List<PostEntity> postList = List.of(dummyPost);
+
+        given(postRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(postList);
+        given(commentRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(Collections.emptyList());
+
+        PostHistory postHistory = mock(PostHistory.class);
+        try (MockedStatic<PostHistory> postHistoryMockStatic = mockStatic(PostHistory.class)) {
+            postHistoryMockStatic.when(() -> PostHistory.from(postList)).thenReturn(postHistory);
+
+            //when
+            userService.getHistory(1L);
+
+            //then
+            postHistoryMockStatic.verify(() -> PostHistory.from(postList), times(1));
+            then(postHistory).should(times(1)).addHistory(any(UserHistoryResponse.class), eq(viewCountService));
+            then(commentRepository).should().findByUserIdAndDate(
+                    anyLong(),
+                    any(LocalDateTime.class),
+                    any(LocalDateTime.class)
+            );
+        }
+    }
+
+    @Test
+    @DisplayName("댓글 이력만 존재하는 경우 CommentHistory.from과 addHistory가 호출되어야 한다.")
+    void getHistoryOnlyComment() {
+        //given
+        CommentEntity dummyComment = CommentEntity.builder().build();
+        List<CommentEntity> dummyList = List.of(dummyComment);
+        given(commentRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(dummyList);
+
+        given(postRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(Collections.emptyList());
+
+        CommentHistory mockCommentHistory = mock(CommentHistory.class);
+        try (MockedStatic<CommentHistory> commentHistoryStatic = mockStatic(CommentHistory.class)) {
+            commentHistoryStatic.when(() -> CommentHistory.from(dummyList))
+                    .thenReturn(mockCommentHistory);
+            //when
+            userService.getHistory(1L);
+
+            //then
+            commentHistoryStatic.verify(() -> CommentHistory.from(dummyList), times(1));
+            then(mockCommentHistory).should().addHistory(any(UserHistoryResponse.class));
+            then(postRepository).should()
+                    .findByUserIdAndDate(
+                            anyLong(),
+                            any(LocalDateTime.class),
+                            any(LocalDateTime.class)
+                    );
+        }
+    }
+
+    @Test
+    @DisplayName("댓글 이력과 게시글 이력이 모두 존재할 경우 addHistory 메서드 모두 호출된다")
+    void getHistoryCommentAndPost() {
+        //given
+        CommentEntity dummyComment = CommentEntity.builder().build();
+        List<CommentEntity> dummyCommentList = List.of(dummyComment);
+        given(commentRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(dummyCommentList);
+
+        PostEntity dummyPost = PostEntity.builder().build();
+        List<PostEntity> dummyPostList = List.of(dummyPost);
+        given(postRepository.findByUserIdAndDate(
+                anyLong(),
+                any(LocalDateTime.class),
+                any(LocalDateTime.class)
+        )).willReturn(dummyPostList);
+
+        try (MockedStatic<CommentHistory> commentHistoryStatic = mockStatic(CommentHistory.class);
+             MockedStatic<PostHistory> postHistoryStatic = mockStatic(PostHistory.class)) {
+
+
+            CommentHistory mockCommentHistory = mock(CommentHistory.class);
+            commentHistoryStatic.when(() -> CommentHistory.from(dummyCommentList))
+                    .thenReturn(mockCommentHistory);
+
+            PostHistory mockPostHistory = mock(PostHistory.class);
+            postHistoryStatic.when(() -> PostHistory.from(dummyPostList))
+                    .thenReturn(mockPostHistory);
+
+            //when
+            userService.getHistory(1L);
+
+            //then
+            commentHistoryStatic.verify(() -> CommentHistory.from(dummyCommentList), times(1));
+            postHistoryStatic.verify(() -> PostHistory.from(dummyPostList), times(1));
+            then(mockCommentHistory).should().addHistory(any(UserHistoryResponse.class));
+            then(mockPostHistory).should().addHistory(any(UserHistoryResponse.class), eq(viewCountService));
+        }
     }
 }
