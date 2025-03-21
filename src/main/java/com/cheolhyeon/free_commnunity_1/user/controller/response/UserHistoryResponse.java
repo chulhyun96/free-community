d55d5040fd84b950c4eff3a258b4e0e4d947c34b
chulhyun96 <park82602@gmail.com>
2025-03-21 17:17:07 +0900

    style :  클래스명 변경

--- src/main/java/com/cheolhyeon/free_commnunity_1/user/controller/response/UserHistoryResponse.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/user/controller/response/UserHistoryResponse.java
@@ -2,22 +2,24 @@ package com.cheolhyeon.free_commnunity_1.user.controller.response;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@ToString
 @Getter
 @NoArgsConstructor
 public class UserHistoryResponse {
-    private final Map<Long, PostHistory> postHistoryMap = new HashMap<>();
-    private final Map<Long, CommentHistory> commentHistoryMap = new HashMap<>();
+    private final Map<Long, PostHistoryResponse> postHistoryMap = new HashMap<>();
+    private final Map<Long, CommentHistoryResponse> commentHistoryMap = new HashMap<>();
 
 
-    public void addPostHistory(PostHistory history) {
+    public void addPostHistory(PostHistoryResponse history) {
         postHistoryMap.put(history.getPostId(), history);
     }
 
-    public void addCommentHistory(CommentHistory history) {
+    public void addCommentHistory(CommentHistoryResponse history) {
         commentHistoryMap.put(history.getCommentId(), history);
     }
 
