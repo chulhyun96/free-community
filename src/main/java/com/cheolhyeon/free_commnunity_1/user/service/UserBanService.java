584572278a216494ff3b0ec32f1fe32d32d9d385
chulhyun96 <park82602@gmail.com>
2025-03-22 23:07:15 +0900

    feat : facade 서비스 레이어 추가

--- src/main/java/com/cheolhyeon/free_commnunity_1/user/service/UserBanService.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/user/service/UserBanService.java
@@ -4,9 +4,25 @@ import com.cheolhyeon.free_commnunity_1.user.repository.RedisUserBanRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
 @RequiredArgsConstructor
 public class UserBanService {
-    private final RedisUserBanRepository redisUserBanRepository;
+    private final RedisUserBanRepository banRedisRepository;
 
+    private static final Duration TTL = Duration.ofDays(3);
+    private static final int THRESHOLD = 5;
+
+    public boolean ban(Long userId, Long reportCount) {
+        if (reportCount >= THRESHOLD) {
+            banRedisRepository.ban(userId, TTL);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isUserBanned(Long userId) {
+        return banRedisRepository.isUserBanned(userId);
+    }
 }
