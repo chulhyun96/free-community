8882caf790255942121c9555eebe16b62ae1f25b
chulhyun96 <park82602@gmail.com>
2025-03-22 14:26:10 +0900

    style : targetId를 writerId로 변경

--- src/main/java/com/cheolhyeon/free_commnunity_1/report/controller/request/ReportRequest.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/report/controller/request/ReportRequest.java
@@ -1,7 +1,5 @@
 package com.cheolhyeon.free_commnunity_1.report.controller.request;
 
-import com.cheolhyeon.free_commnunity_1.report.type.ReportReason;
-import com.cheolhyeon.free_commnunity_1.report.type.ReportType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,19 +7,18 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ReportRequest {
-    private Long reportId;
-    private Long targetId;
-    private ReportReason reason;
-    private ReportType reportType;
+    private Long reporterId;
+    private Long writerId;
+    private Long contentId;
+    private String reason;
+    private String reportType;
 
     @Builder
-    private ReportRequest(Long reportId, Long targetId, ReportReason reason, ReportType reportType) {
-        this.reportId = reportId;
-        this.targetId = targetId;
+    public ReportRequest(Long reporterId, Long writerId, Long contentId, String reason, String reportType) {
+        this.reporterId = reporterId;
+        this.writerId = writerId;
+        this.contentId = contentId;
         this.reason = reason;
         this.reportType = reportType;
-    }
-    public static ReportRequest of(Long reportId, Long targetId, ReportReason reason, ReportType reportType) {
-        return new ReportRequest(reportId, targetId, reason, reportType);
     }
 }
