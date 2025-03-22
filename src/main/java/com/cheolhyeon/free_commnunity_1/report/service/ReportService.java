9f50a798372e2f8465b57b5ab0d16b9eae3e5b4a
chulhyun96 <park82602@gmail.com>
2025-03-22 14:27:28 +0900

    feat : ReportType에 따라 분기점

--- src/main/java/com/cheolhyeon/free_commnunity_1/report/service/ReportService.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/report/service/ReportService.java
@@ -1,9 +1,58 @@
 package com.cheolhyeon.free_commnunity_1.report.service;
 
+import com.cheolhyeon.free_commnunity_1.comment.repository.CommentRepository;
+import com.cheolhyeon.free_commnunity_1.comment.repository.entity.CommentEntity;
+import com.cheolhyeon.free_commnunity_1.post.repository.PostRepository;
+import com.cheolhyeon.free_commnunity_1.post.repository.entity.PostEntity;
+import com.cheolhyeon.free_commnunity_1.report.controller.request.ReportRequest;
+import com.cheolhyeon.free_commnunity_1.report.controller.response.ReportResponse;
+import com.cheolhyeon.free_commnunity_1.report.repository.ReportRepository;
+import com.cheolhyeon.free_commnunity_1.report.repository.entity.ReportEntity;
+import com.cheolhyeon.free_commnunity_1.report.type.ReportReason;
+import com.cheolhyeon.free_commnunity_1.report.type.ReportType;
+import com.cheolhyeon.free_commnunity_1.user.repository.UserRepository;
+import com.cheolhyeon.free_commnunity_1.user.repository.entity.UserEntity;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+import java.time.Duration;
+
+@Service
+@Transactional
 @RequiredArgsConstructor
 public class ReportService {
+    private static final Duration TTL = Duration.ofDays(1);
+
+    private final ReportRepository reportRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ReportRedisRepository reportRedisRepository;
+
+
+    public ReportResponse report(ReportRequest request) {
+        ReportType type = ReportType.from(request.getReportType());
+        ReportReason reason = ReportReason.from(request.getReason());
+        ReportEntity entity = reportRepository.save(ReportEntity.from(request, type, reason));
+        // type이 POST인지 확인
+        if (type == ReportType.POST) {
+            postRepository.findById(request.getContentId())
+                    .ifPresentOrElse(PostEntity::reported, () -> {
+                        throw new EntityNotFoundException("해당 게시글을 찾을 수 없습니다");
+                    });
+        }
+        // type이 COMMENT인지 확인
+        if (type == ReportType.COMMENT) {
+            commentRepository.findById(request.getContentId())
+                    .ifPresentOrElse(CommentEntity::reported, () -> {
+                        throw new EntityNotFoundException("해당 댓글을 찾을 수 없습니다");
+                    });
+        }
+        UserEntity userEntity = userRepository.findById(request.getWriterId())
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저를 찾을 수 없습니다"));
+        reportRedisRepository.report(userEntity.getId(), TTL);
+        return ReportResponse.from(entity);
+    }
 }
