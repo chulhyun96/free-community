a45eb09535417c80f19959b779aefb68e71ede81
chulhyun96 <park82602@gmail.com>
2025-03-22 20:34:47 +0900

    feat : report API 엔드포인트

--- src/main/java/com/cheolhyeon/free_commnunity_1/report/controller/ReportController.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/report/controller/ReportController.java
@@ -1,11 +1,22 @@
 package com.cheolhyeon.free_commnunity_1.report.controller;
 
+import com.cheolhyeon.free_commnunity_1.report.controller.request.ReportRequest;
+import com.cheolhyeon.free_commnunity_1.report.controller.response.ReportResponse;
 import com.cheolhyeon.free_commnunity_1.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ReportController {
     private final ReportService reportService;
+
+    @PostMapping("/report")
+    public ResponseEntity<ReportResponse> report(
+            @RequestBody ReportRequest request) {
+        return ResponseEntity.ok(reportService.report(request));
+    }
 }
