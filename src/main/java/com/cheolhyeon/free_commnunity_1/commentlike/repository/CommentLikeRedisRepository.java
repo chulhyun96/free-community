0c17a07152f37a4b914e1a4fd53faa04f2e103c9
chulhyun96 <park82602@gmail.com>
2025-03-29 22:07:03 +0900

    refactor : Redis에서 get으로 조회하던 로직을 multiGet로직으로 변경

--- src/main/java/com/cheolhyeon/free_commnunity_1/commentlike/repository/CommentLikeRedisRepository.java
+++ src/main/java/com/cheolhyeon/free_commnunity_1/commentlike/repository/CommentLikeRedisRepository.java
@@ -4,6 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 @Repository
 @RequiredArgsConstructor
 public class CommentLikeRedisRepository {
@@ -20,13 +25,24 @@ public class CommentLikeRedisRepository {
         return commentLikeRedisTemplate.opsForValue().decrement(key);
     }
 
-    public Long getCurrentCommentLikeCount(Long postId,Long commentId) {
-        String key = generateKey(postId, commentId);
-        String result = commentLikeRedisTemplate.opsForValue().get(key);
-        return result == null ? 0L : Long.parseLong(result);
+    public Map<Long, Long> getCurrentCommentLikeCount(Long postId, List<Long> commentIds) {
+        List<String> keys = commentIds.stream()
+                .map(commentId -> generateKey(postId, commentId))
+                .toList();
+
+        List<String> results = commentLikeRedisTemplate.opsForValue().multiGet(keys);
+
+        Map<Long, Long> likeCounts = new HashMap<>();
+        for (int i = 0; i < commentIds.size(); i++) {
+            Long commentId = commentIds.get(i);
+            String raw = Objects.requireNonNull(results).get(i);
+            Long count = (raw == null) ? 0L : Long.parseLong(raw);
+            likeCounts.put(commentId, count);
+        }
+        return likeCounts;
     }
 
-    private String generateKey(Long postId,Long commentId) {
+    private String generateKey(Long postId, Long commentId) {
         return KEY.formatted(postId, commentId);
     }
 
