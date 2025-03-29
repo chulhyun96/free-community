8ce2d31e1b6c6a63c1ae4be50dc014d4a0318cc0
chulhyun96 <park82602@gmail.com>
2025-03-29 22:07:32 +0900

    test : Redis에서 get으로 조회하던 로직을 multiGet로직으로 변경에 따른 테스트 코드 수정

--- src/test/java/com/cheolhyeon/free_commnunity_1/commentlike/repository/CommentLikeRedisRepositoryTest.java
+++ src/test/java/com/cheolhyeon/free_commnunity_1/commentlike/repository/CommentLikeRedisRepositoryTest.java
@@ -9,7 +9,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -33,14 +36,12 @@ class CommentLikeRedisRepositoryTest {
         // Given
         given(commentLikeRedisTemplate.opsForValue()).willReturn(valueOperations);
         given(valueOperations.increment(anyString())).willReturn(1L);
-        given(valueOperations.get(anyString())).willReturn("1");
-
+        given(commentLikeRedisTemplate.opsForValue().multiGet(anyList())).willReturn(List.of("1"));
         // When
         likeRedisRepository.increment(1L, 1L);
-        Long currentCommentLikeCount = likeRedisRepository.getCurrentCommentLikeCount(1L, 1L);
+        Map<Long, Long> currentCommentLikeCount = likeRedisRepository.getCurrentCommentLikeCount(1L, List.of(1L));
 
         // Then
-        assertThat(currentCommentLikeCount).isEqualTo(1L);
         then(valueOperations).should().increment(anyString());
     }
     @Test
@@ -49,14 +50,13 @@ class CommentLikeRedisRepositoryTest {
         // Given
         given(commentLikeRedisTemplate.opsForValue()).willReturn(valueOperations);
         given(valueOperations.decrement(anyString())).willReturn(1L);
-        given(valueOperations.get(anyString())).willReturn("1");
+        given(commentLikeRedisTemplate.opsForValue().multiGet(anyList())).willReturn(List.of("1"));
 
         // When
         likeRedisRepository.decrement(1L,1L);
-        Long currentCommentLikeCount = likeRedisRepository.getCurrentCommentLikeCount(1L,1L);
+        Map<Long, Long> currentCommentLikeCount = likeRedisRepository.getCurrentCommentLikeCount(1L, List.of(1L));
 
         // Then
-        assertThat(currentCommentLikeCount).isEqualTo(1L);
         then(valueOperations).should().decrement(anyString());
     }
 
@@ -65,11 +65,11 @@ class CommentLikeRedisRepositoryTest {
     void getCurrentCommentLikeCount() {
         //given
         given(commentLikeRedisTemplate.opsForValue()).willReturn(valueOperations);
-        given(valueOperations.get(anyString())).willReturn("100");
+        given(commentLikeRedisTemplate.opsForValue().multiGet(anyList())).willReturn(List.of("1"));
+
         //when
-        Long currentCommentLikeCount = likeRedisRepository.getCurrentCommentLikeCount(1L,1L);
+        Map<Long, Long> currentCommentLikeCount = likeRedisRepository.getCurrentCommentLikeCount(1L, List.of(1L));
         //then
-        assertThat(currentCommentLikeCount).isEqualTo(100);
-        then(valueOperations).should(times(1)).get(anyString());
+        then(valueOperations).should(times(1)).multiGet(anyList());
     }
 }
